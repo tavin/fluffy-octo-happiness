825767d8240b1150d263a05770e6a3cb7d12920f
Tavin Cole <tavin.cole@gmail.com>
2020-08-22 16:53:40 +0200

    wip

--- vars/abort.groovy
+++ vars/abort.groovy
@@ -1,23 +1,5 @@
-import com.cloudbees.groovy.cps.NonCPS
 import hudson.model.Result
-import jenkins.model.CauseOfInterruption
-import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
-
-class ScriptAbort extends CauseOfInterruption {
-
-    private String message
-
-    ScriptAbort(String message) {
-        this.message = message
-    }
-
-    String getShortDescription() {
-        return message
-    }
-}
 
 void call(String message) {
-    withContext {
-        throw new FlowInterruptedException(Result.ABORTED, new ScriptAbort(message))
-    }
+    currentBuild.getRawBuild().getExecutor().interrupt(Result.ABORTED)
 }
